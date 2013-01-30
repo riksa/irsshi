c3a0374481e269f7964cbe3c4e7f3dd4c56d6e4e
riku salkia <riksa@iki.fi>
2013-01-30 12:17:03 +0200

    added slf4j dependency

--- android-irsshi/src/main/java/org/riksa/irsshi/HelloAndroidActivity.java
+++ android-irsshi/src/main/java/org/riksa/irsshi/HelloAndroidActivity.java
@@ -2,23 +2,26 @@ package org.riksa.irsshi;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloAndroidActivity extends Activity {
-
-    private static String TAG = "android-irsshi";
+    private static String TAG = "irsshi-";
+    private static final Logger log = LoggerFactory.getLogger(TAG + HelloAndroidActivity.class.getSimpleName());
 
     /**
      * Called when the activity is first created.
-     * @param savedInstanceState If the activity is being re-initialized after 
-     * previously being shut down then this Bundle contains the data it most 
-     * recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *                           previously being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		Log.i(TAG, "onCreate");
+        log.debug("Hello {}", "world");
         setContentView(R.layout.main);
+
     }
 
 }
